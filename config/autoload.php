94fae7b3524ce0dcdba874a203dc8b9cb7218fe9
Yanick Witschi <yanick.witschi@terminal42.ch>
2012-09-13 09:55:08 +0200

    ah yeah, here comes contao 3 compatibility

---
+++ config/autoload.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package Easy_themes
+ * @link    http://www.contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	// Easy_themes
+	'CheckBoxChooseAtLeastOne' => 'system/modules/easy_themes/CheckBoxChooseAtLeastOne.php',
+	'EasyThemes'               => 'system/modules/easy_themes/EasyThemes.php',
+));
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'be_easythemes' => 'system/modules/easy_themes/templates',
+));
